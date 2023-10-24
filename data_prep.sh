d506ae71e7e12ed011d8e4ccb6a786c6187fe5e5
Hiroyuki KURATA <37858243+kuratahiroyuki@users.noreply.github.com>
2023-10-24 16:07:04 +0900

    Add files via upload

---
+++ data_prep.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+program_path=$(pwd)
+data_path=${program_path}/data/dataset
+#infile1=${data_path}/train-D-mm.txt
+#infile2=${data_path}/ind-D-mm.txt
+
+outfile1=${data_path}/train_D.txt
+outfile2=${data_path}/test_D.txt
+
+test_fasta=${data_path}/independent_test/independent_test.fa
+test_csv=${data_path}/independent_test/independent_test.csv
+kfold=5
+
+
+#python ${program_path}/formatting.py --infile1 ${infile1} --infile2 ${infile2} --outfile1 ${outfile1} --outfile2 ${outfile2}
+
+python ${program_path}/train_division.py --infile1 ${outfile1} --datapath ${data_path} --kfold ${kfold} 
+
+python ${program_path}/test_fasta.py --infile1 ${outfile2} --outfile1 ${test_fasta} --outfile2 ${test_csv} 
+
+
+
